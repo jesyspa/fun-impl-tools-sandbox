a1996eb55bab172b34e67d3b69413782cbd9f6e2
Anton Golov <jesyspa@gmail.com>
2015-05-05 21:39:29 +0200

    Implemented lambda pretty-printing.

--- Main.hs
+++ Main.hs
@@ -5,7 +5,7 @@ import Control.Monad
 
 handleParseError :: (a -> IO ()) -> Either ParseError a -> IO ()
 handleParseError _ (Left err) = print err
-handleParseError go (Right a)  = go a
+handleParseError go (Right a) = go a
 
 main :: IO ()
 main = forever $ handleParseError (putStrLn . pprint) . parseString top "<stdin>" =<< getLine
--- PrettyPrinter/Simple.hs
+++ PrettyPrinter/Simple.hs
@@ -1,8 +1,12 @@
 module PrettyPrinter.Simple (pprint) where
 
+import Bound
+import Control.Monad.State (evalState)
 import Text.PrettyPrint
 import AST.Simple hiding (App)
 import qualified AST.Simple as AST (AST(App))
+import Tools.Gen
+import Control.Applicative
 
 data ParenLevel = None | Sum | Mul | App
                 deriving (Eq, Ord, Read, Show)
@@ -11,17 +15,39 @@ parensIf :: Bool -> Doc -> Doc
 parensIf True x = parens x
 parensIf False x = x
 
-pprintPL :: ParenLevel -> AST String -> Doc
-pprintPL _ (Var a) = text a
-pprintPL _ (NumLit n) = int n
-pprintPL _ (Lam _) = text "lambda"
-pprintPL l (BinOp Plus x y) = parensIf (l > None) $ pprintPL None x <+> char '+' <+> pprintPL Sum y
-pprintPL l (BinOp Minus x y) = parensIf (l > None) $ pprintPL None x <+> char '-' <+> pprintPL Sum y
-pprintPL l (BinOp Times x y) = parensIf (l > Sum) $ pprintPL Sum x <+> char '*' <+> pprintPL Mul y
-pprintPL l (BinOp Divide x y) = parensIf (l > Sum) $ pprintPL Sum x <+> char '/' <+> pprintPL Mul y
-pprintPL l (AST.App x y) = parensIf (l > Mul) $ pprintPL Mul x <+> pprintPL App y
-pprintPL l (IfZ x y z) = parensIf (l > None) $ text "if" <+> pprintPL None x <+> text "then" <+> pprintPL None y <+> text "else" <+> pprintPL None z
+infixl 4 <*+*>
+(<*+*>) :: Applicative f => f Doc -> f Doc -> f Doc
+(<*+*>) = liftA2 (<+>)
+
+char' :: Applicative f => Char -> f Doc
+char' = pure . char
+text' :: Applicative f => String -> f Doc
+text' = pure . text
+int' :: Applicative f => Int -> f Doc
+int' = pure . int
+
+showLambda :: Scope () AST Doc -> Gen Int Doc
+showLambda e = do
+    n <- fresh
+    let name = text "$x_" <> int n
+        e' = instantiate1 (Var name) e
+    body <- pprintPL None e'
+    return $ char '\\' <> name <+> text "->" <+> body
+
+pprintPL :: ParenLevel -> AST Doc -> Gen Int Doc
+pprintPL _ (Var a) = pure a
+pprintPL _ (NumLit n) = int' n
+pprintPL l (Lam e) = parensIf (l > None) <$> showLambda e
+pprintPL l (BinOp Plus x y) =  parensIf (l > None) <$> pNone x <*+*> char' '+' <*+*> pprintPL Sum y
+pprintPL l (BinOp Minus x y) =  parensIf (l > None) <$> pNone x <*+*> char' '-' <*+*> pprintPL Sum y
+pprintPL l (BinOp Times x y) =  parensIf (l > Sum) <$> pprintPL Sum x <*+*> char' '*' <*+*> pprintPL Mul y
+pprintPL l (BinOp Divide x y) = parensIf (l > Sum) <$> pprintPL Sum x <*+*> char' '/' <*+*> pprintPL Mul y
+pprintPL l (AST.App x y) = parensIf (l > Mul) <$> pprintPL Mul x <*+*> pprintPL App y
+pprintPL l (IfZ x y z) = parensIf (l > None) <$> text' "if" <*+*> pNone x <*+*> text' "then" <*+*> pNone y <*+*> text' "else" <*+*> pNone z
+
+pNone :: AST Doc -> Gen Int Doc
+pNone = pprintPL None
 
 pprint :: AST String -> String
-pprint = render . pprintPL None
+pprint x = render $ evalState (pNone $ text <$> x) 0
 
